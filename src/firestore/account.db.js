7afa812bc44be0e5ece6e28b4717cd674621fef0
dills122 <dylansteele57@gmail.com>
2020-01-16 18:20:53 -0500

    created rest of the services and updated protos

--- src/firestore/account.db.js
+++ src/firestore/account.db.js
@@ -1,23 +1,42 @@
 const dbFactory = require("./db-factory");
+const uuid = require("uuid/v4");
+const _ = require("lodash");
 const collection = "accounts";
 const db = dbFactory();
 
 module.exports = {
-    Create: (model) => {
-        return db.collection(collection).doc(model.bankId).set(model);
-    },
-    Get: (bankId) => {
-        return db.collection(collection).doc(bankId).get();
-    },
-    Archive: (bankId) => {
-        db.collection(collection).doc(bankId).update({
-            archive: true
-        });
-    },
-    UpdateBalance: async (bankId, amount) => {
-        let record = await this.Get(bankId);
-        db.collection(collection).doc(bankId).update({
-            balance: record.data.balance + amount
-        });
-    }
+  Create: async model => {
+    let bankId = uuid();
+    let { clientId, beginningBalance } = model;
+    let obj = { bankId, clientId, balance: beginningBalance };
+    await db
+      .collection(collection)
+      .doc(`${bankId}`)
+      .set(obj);
+    return obj;
+  },
+  Get: async bankId => {
+    let doc = await db
+      .collection(collection)
+      .doc(bankId)
+      .get();
+    return doc.data();
+  },
+  Archive: bankId => {
+    db.collection(collection)
+      .doc(`${bankId}`)
+      .update({
+        archive: true
+      });
+  },
+  UpdateBalance: async (bankId, amount) => {
+    let record = (await this.Get(bankId)).data();
+    let updatedBalance = _.get(record, "balance", 0) + amount;
+    db.collection(collection)
+      .doc(`${bankId}`)
+      .update({
+        balance: updatedBalance
+      });
+    return { updatedBalance, ...record };
+  }
 };
